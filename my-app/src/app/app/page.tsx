a5e6995bf0050c29c315bbba22e1665a75706ce4
surajgoraicse <surajgoraicse@gmail.com>
2024-10-29 10:40:03 +0530

    setup next project

---
+++ my-app/src/app/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+    return (
+        <>
+            <h1 className="text-4xl font-bold text-center py-4 text-blue-300 underline">This is my app</h1></>
+    );
+
+}
+
+export default App;
--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -28,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className=" font-bold text-2xl">this is a Header</h1>
         {children}
+        <h1 className=" font-bold text-2xl">this is a footer</h1>
+
       </body>
     </html>
   );
